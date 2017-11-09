6614d48768257d73af4fb6db63e63e3283450a17
DESKTOP-D7JVEFC\onasu <filmzaza23@gmail.com>
2017-11-10 01:35:07 +0700

    login js

--- ProjactElectric/js/login.js
+++ ProjactElectric/js/login.js
@@ -1,1 +1,24 @@
+var config = {
+    apiKey: "AIzaSyB2_l8HIycLAdM6a9vA9XMaS_VPqxmBK1o",
+    authDomain: "datacontro.firebaseapp.com",
+    databaseURL: "https://datacontro.firebaseio.com",
+    projectId: "datacontro",
+    storageBucket: "datacontro.appspot.com",
+    messagingSenderId: "585589132177"
+  };
+  
+ 
 
+function signin(){
+
+    var email = document.getElementById('txt-email').val;
+    var password = document.getElementById('txt-pass').val;
+
+    if(email != "" && password != ""){
+        alert("error");
+        console.log('error');
+
+    }
+
+
+})
