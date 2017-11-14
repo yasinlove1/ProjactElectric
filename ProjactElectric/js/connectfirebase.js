f29d286a67508a6ae92e04842ac6e58c19b6968a
yasinlove1 <sakchaichammalee@gmail.com>
2017-11-14 21:15:32 +0700

    add status s102

--- ProjactElectric/js/connectfirebase.js
+++ ProjactElectric/js/connectfirebase.js
@@ -1,12 +1,13 @@
-var config = {
+
+  var config = {
     apiKey: "AIzaSyB2_l8HIycLAdM6a9vA9XMaS_VPqxmBK1o",
     authDomain: "datacontro.firebaseapp.com",
     databaseURL: "https://datacontro.firebaseio.com",
     projectId: "datacontro",
     storageBucket: "datacontro.appspot.com",
     messagingSenderId: "585589132177"
-};
-firebase.initializeApp(config);
+  };
+  firebase.initializeApp(config);
 
 var dbFirebase = firebase.database();
 var dbRef = firebase.database().ref()
@@ -59,3 +60,56 @@ var numAIR1R1s = numAIR1R1.on('value', function (snapshot) {
     }
     console.log('R1S101_AIR2:' + snapAIR1R1)
 });
+
+var numLEDR2 = dbFirebase.ref('room2/UserinRoom');
+var numLEDR2s = numLEDR2.on('value',function(snapshot){
+
+    var snapLEDR2 = snapshot.val();
+    var num = 0
+    var LED;
+    if(snapLEDR2 == num){
+        $("#btn-LED-R2").addClass('btn-off');
+        $("#btn-LED-R2").removeClass('btn-on');
+    }else{
+        $("#btn-LED-R2").addClass('btn-on');
+        $("#btn-LED-R2").removeClass('btn-off');
+}
+    console.log('R2S102_LED:'+snapLEDR2)
+    $('#userinRoomS102').text(snapLEDR2);
+});
+var numAIR0R2 = dbFirebase.ref("room2/air");
+var numAIR0R2s = numAIR0R2.on('value',function(snapshot){
+    var snapAIR0R2 = snapshot.val();
+    var num = 0
+    var AIR;
+    if(snapAIR0R2 == num){
+        $("#btn-AIR1-R2").addClass('btn-off');
+        $("#btn-AIR1-R2").removeClass('btn-on');
+    }else{
+        $("#btn-AIR1-R2").addClass('btn-on');
+        $("#btn-AIR1-R2").removeClass('btn-off');
+    }
+    console.log('R1S102_AIR1:'+ snapAIR0R2)
+});
+
+var numAIR1R2 = dbFirebase.ref('room2/air1');
+var numAIR1R2s = numAIR1R2.on('value',function(snapshot){
+    var snapAIR1R2 = snapshot.val();
+    var num = 0
+    var AIR;
+    if(snapAIR1R2 == num){
+        $("#btn-AIR2-R2").addClass('btn-off');
+        $("#btn-AIR2-R2").removeClass('btn-on');
+ } else{
+    $("#btn-AIR2-R2").addClass('btn-on');
+    $("#btn-AIR2-R2").removeClass('btn-off');
+ }
+ console.log('R1S102_AiR2:'+snapAIR1R2)
+});
+
+
+
+
+
+
+
